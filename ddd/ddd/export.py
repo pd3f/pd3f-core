51ec6da2cb33e34da2adb65907122ce6ccb43b07
Johannes Filter <hi@jfilter.de>
2020-06-18 19:30:09 +0200

    detect footnotes

--- ddd/ddd/export.py
+++ ddd/ddd/export.py
@@ -4,6 +4,8 @@ from collections import Counter
 
 from .utils import flatten
 
+# TODO: What to do with list?
+
 # see this for more
 # https://github.com/axa-group/Parsr/blob/365ad388fd5dc7ff9c3fa7db28f45460baa899b0/server/src/output/markdown/MarkdownExporter.ts
 
@@ -29,7 +31,9 @@ class Export:
         self.remove_header = remove_header
         self.remove_footer = remove_footer
 
-        self.font_stats()
+        self.overall_font_stats()
+        self.para_stats()
+
         self.export()
 
     def export(self):
@@ -51,14 +55,28 @@ class Export:
                 if element["type"] == "heading":
                     cleaned_data.append(self.export_heading(element))
                 if element["type"] == "paragraph":
-                    cleaned_data.append(self.export_paragraph(element))
+                    cleaned_data.append(
+                        self.export_paragraph(element, page["pageNumber"])
+                    )
 
         self.export_data = cleaned_data
 
-    def font_stats(self):
-        """Get statistics about font usage
-        """
+    def para_stats(self):
+        self.page_para_order = []
+        for p in self.input_data["pages"]:
+            per_page = []
+            for e in p["elements"]:
+                if not e["type"] == "paragraph":
+                    continue
+                if "isHeader" in e["properties"] and e["properties"]["isHeader"]:
+                    continue
+                if "isFooter" in e["properties"] and e["properties"]["isFooter"]:
+                    continue
 
+                per_page.append(e["id"])
+            self.page_para_order.append(per_page)
+
+    def font_stats(self, e):
         def traverse(element):
             if type(element) is dict:
                 if "font" in element:
@@ -69,12 +87,20 @@ class Export:
             if type(element) is list:
                 return [traverse(e) for e in element]
 
+        return [x for x in flatten(traverse(e)) if x is not None]
+
+    def overall_font_stats(self):
+        """Get statistics about font usage
+        """
         c = Counter()
         for p in self.input_data["pages"]:
             for e in p["elements"]:
-                res = [x for x in flatten(traverse(e)) if x is not None]
-                c.update(res)
+                c.update(self.font_stats(e))
         self.font_counter = c
+        self.font_info = {}
+        for x in self.input_data["fonts"]:
+            self.font_info[x["id"]] = x
+            assert x["sizeUnit"] == "px"
         print(c)
 
     def add_linebreak(self, line, next_line, paragraph):
@@ -85,32 +111,66 @@ class Export:
         )
         return available_space >= next_line["content"][0]["box"]["w"]
 
-    def line_to_words(self, line, as_string=False):
+    def line_to_words(self, line):
         results = []
+        fonts = []
         for word in line["content"]:
             if word["type"] == "word":
                 results.append(word["content"])
+                fonts.append(word["font"])
+        return results, fonts
 
-        if as_string:
-            return " ".join(results)
-        return results
+    def lines_to_paragraph(self, paragraph, page_number):
+        raw_lines = paragraph["content"]
+        font_counter = Counter()
+        lines = []
 
-    def lines_to_paragraph(self, paragraph):
-        lines = paragraph["content"]
-        string_lines = [self.line_to_words(l) for l in lines]
+        for l in raw_lines:
+            rl, rf = self.line_to_words(l)
+            lines.append(rl)
+            font_counter.update(rf)
 
-        if self.detect_footnotes(paragraph):
-            pass
+        if self.is_footnote_paragraph(paragraph, font_counter, page_number):
+            # don't test on last line
+            for i in range(0, len(lines) - 1):
+                # decide whether newline or simple space
+                if self.add_linebreak(raw_lines[i], raw_lines[i + 1], paragraph):
+                    lines[i].append("\n")
+                else:
+                    # if the first chars are digits -> footnote
+                    if lines[i][0].isnumeric() and lines[i + 1][0].isnumeric():
+                        lines[i].append("\n")
+                    else:
+                        lines[i].append(" ")
 
-        # don't test on last line
-        for i in range(0, len(lines) - 1):
-            # decide whether newline or simple space
-            if self.add_linebreak(lines[i], lines[i + 1], paragraph):
-                string_lines[i].append("\n")
-            else:
-                string_lines[i].append(" ")
+            return {"lines": lines, "type": "footnotes"}
+            print("suup")
+        else:
+            # don't test on last line
+            for i in range(0, len(lines) - 1):
+                # decide whether newline or simple space
+                if self.add_linebreak(raw_lines[i], raw_lines[i + 1], paragraph):
+                    lines[i].append("\n")
+                else:
+                    lines[i].append(" ")
 
-        return {"lines": string_lines, "type": "body"}
+            return {"lines": lines, "type": "body"}
+
+    def export_heading(self, e):
+        raw_lines = e["content"]
+        lines = []
+        for l in raw_lines:
+            rl, _ = self.line_to_words(l)
+            lines.append(rl)
+
+        return {
+            "lines": lines,
+            "type": "heading",
+            "level": e["level"],
+        }
+
+    def export_paragraph(self, e, page_number):
+        return self.lines_to_paragraph(e, page_number)
 
     def to_markdown(self):
         return self.to_text(markdown=True)
@@ -123,17 +183,10 @@ class Export:
                 txt += "#" * l["level"] + " "
             if l["type"] == "body" or l["type"] == "heading":
                 txt += "".join([" ".join(line) for line in l["lines"]]) + "\n\n"
+
+            if l["type"] == "footnotes":
+                txt += "".join([" ".join(line) for line in l["lines"]]) + "\n\n"
         return txt
-
-    def export_heading(self, e):
-        return {
-            "lines": self.lines_to_paragraph(e)["lines"],
-            "type": "heading",
-            "level": e["level"],
-        }
-
-    def export_paragraph(self, e):
-        return self.lines_to_paragraph(e)
 
     def save_text(self, output_path):
         Path(output_path).write_text(self.to_text())
@@ -141,9 +194,20 @@ class Export:
     def save_markdown(self, output_path):
         Path(output_path).write_text(self.to_markdown())
 
-    def detect_footnotes(self, paragraph):
-        pass
+    def is_footnote_paragraph(self, paragraph, counter, page_number):
+        # TODO: more heuristic: 1. do numbers appear in text? 2. is there a drawing in it
 
-    # based on font: (smaller)
-    # location bottom
+        para_font = counter.most_common(1)[0][0]
+        body_font = self.font_counter.most_common(1)[0][0]
+
+        # footnotes has to be different
+        if para_font == body_font:
+            return False
+
+        # footnotes has to be smaller
+        if self.font_info[para_font]["size"] > self.font_info[body_font]["size"]:
+            return False
+
+        # check if this is the last paragraph
+        return self.page_para_order[page_number - 1][-1] == paragraph["id"]
 
