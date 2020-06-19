a2fde3a6aec63238d4fdca2ac77b21ad748bc106
Johannes Filter <hi@jfilter.de>
2020-06-19 21:10:13 +0200

    pin parsr version

--- scripts/start_parsr.sh
+++ scripts/start_parsr.sh
@@ -5,12 +5,14 @@ set -x
 # set ssh config
 remote_destination=$REMOTE_WORKSTATION
 
+parsr_version="v0.12.2"
+
 if [ "$1" = "stop" ]; then
-  ssh $remote_destination "docker stop parsr-api && docker stop parsr-ui"
+  ssh $remote_destination "docker stop parsr-api; docker stop parsr-ui" || true
   killall ssh
 else
-  ssh -f $remote_destination "nohup docker run --name parsr-api --rm -p 3001:3001 axarev/parsr:latest > /dev/null 2>&1 &"
-  ssh -f $remote_destination "nohup docker run --name parsr-ui --rm -t -p 8080:80 axarev/parsr-ui-localhost:latest > /dev/null 2>&1 &"
+  ssh -f $remote_destination "nohup docker run --name parsr-api --rm -p 3001:3001 axarev/parsr:$parsr_version > /dev/null 2>&1 &"
+  ssh -f $remote_destination "nohup docker run --name parsr-ui --rm -t -p 8080:80 axarev/parsr-ui-localhost:$parsr_version > /dev/null 2>&1 &"
   ssh -f -N -L 8080:localhost:8080 $remote_destination
   ssh -f -N -L 3001:localhost:3001 $remote_destination
 fi
