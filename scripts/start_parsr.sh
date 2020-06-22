d5345352d0cd7254b4eb9104cbbac1b369de4c11
Johannes Filter <hi@jfilter.de>
2020-06-22 22:57:19 +0200

    use specific version for UI

--- scripts/start_parsr.sh
+++ scripts/start_parsr.sh
@@ -5,14 +5,16 @@ set -x
 # set ssh config
 remote_destination=$REMOTE_WORKSTATION
 
-parsr_version="v0.12.2"
+# had to user different versions to make it work
+parsr_version="v0.12"
+parsr_ui_version="v0.12.2"
 
 if [ "$1" = "stop" ]; then
   ssh $remote_destination "docker stop parsr-api; docker stop parsr-ui" || true
   killall ssh
 else
   ssh -f $remote_destination "nohup docker run --name parsr-api --rm -p 3001:3001 axarev/parsr:$parsr_version > /dev/null 2>&1 &"
-  ssh -f $remote_destination "nohup docker run --name parsr-ui --rm -t -p 8080:80 axarev/parsr-ui-localhost:$parsr_version > /dev/null 2>&1 &"
+  ssh -f $remote_destination "nohup docker run --name parsr-ui --rm -t -p 8080:80 axarev/parsr-ui-localhost:$parsr_ui_version > /dev/null 2>&1 &"
   ssh -f -N -L 8080:localhost:8080 $remote_destination
   ssh -f -N -L 3001:localhost:3001 $remote_destination
 fi
