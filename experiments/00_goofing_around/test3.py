b29d8e7f253e19c5cf14097c17f16d342178b045
Johannes Filter <hi@jfilter.de>
2020-06-23 18:18:11 +0200

    add goofing around experiments

---
+++ experiments/00_goofing_around/test3.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import ddd
+
+for p in Path("../data/bmjv/").glob("*.pdf"):
+    if p.name.startswith("00003"):
+        print(p.name)
+        ddd.do_parsr(
+            str(p),
+            out_dir="out/",
+            cleaner_config=[["reading-order-detection", {"minVerticalGapWidth": 20}]],
+        )
+
+        e = ddd.Export(f"out/{p.stem}/data.json")
+        e.save_markdown(f"out/{p.stem}/clean.md")
+
+
+# run.do_parsr(
+#     "../data/bmjv/00001_112018_FU_Berlin_Richtlinie_2017_1371.pdf",
+#     out_dir="out2",
+#     cleaner_config=[["reading-order-detection", {"minVerticalGapWidth": 20}]],
+# )
+
+
+# run.do_parsr(
+#     "../data/bmjv/01384_011020_Stellungnahme_DRB_RefE__Belaempfung-Rechtsextremismus-Hasskriminalitaet.pdf",
+#     out_dir="out1",
+#     cleaner_config=[["reading-order-detection", {"minVerticalGapWidth": 10}]],
+# )
+
+
+# output.save(
+#     "out1/01384_011020_Stellungnahme_DRB_RefE__Belaempfung-Rechtsextremismus-Hasskriminalitaet/data.json",
+#     "d.txt",
+# )
